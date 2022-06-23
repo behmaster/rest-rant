c6b577dce005c257d7e858a20ce15566a07b0e8f
Robbie Behm <behmaster@gmail.com>
2022-06-22 21:14:19 -0500

    helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,13 +7,19 @@ function show (data) {
           <main>
             <h1>{ data.place.name }</h1>
             <img src={data.place.pic} alt={data.place.name} />
+            <h3>
+                Located in {data.place.city}, {data.place.state}
+            </h3>
             <div>
                 <h2>Rating</h2>
                 <p>not rated</p>
             </div>
             <div>
                 <h2>Description</h2>
-                <p>Located in {data.place.city}, {data.place.state} and serving {data.place.cuisines} from {data.place.yearFounded}</p>
+                <h3>
+                    {data.place.showEstablished()}
+                </h3>
+                <p>serving {data.place.cuisines}</p>
             </div>
             <div>
                 <h2>Comments</h2>
