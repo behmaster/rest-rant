ec835cd4390624d8cc82d1331e749c5e2782e516
Robbie Behm <behmaster@gmail.com>
2022-06-22 21:29:07 -0500

    validations

--- models/places.js
+++ models/places.js
@@ -2,11 +2,15 @@ const mongoose = require("mongoose");
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic: { type: String, default: 'http://placekitten.com/350/350' },
   cuisines: { type: String, required: true },
   city: { type: String, default: "Anytown" },
   state: { type: String, default: "USA" },
-  founded: Number,
+  founded: {
+    type: Number,
+    min: [1900, "that's an old restaurant"],
+    max: [new Date().getFullYear(), "are you Marty Mcfly?"]
+  }
 });
 
 placeSchema.methods.showEstablished = function() {
